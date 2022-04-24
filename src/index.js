b80c2e61dd349d084d8efe5eb6668f20b6ffe249
Tushar Rawat <48175982+ankittushar@users.noreply.github.com>
2022-04-24 12:21:22 +0530

    title

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import ProgressBar from 'react-bootstrap/esm/ProgressBar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
